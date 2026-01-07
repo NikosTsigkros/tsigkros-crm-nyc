af7770fb5728fad2b32374098353c0f75bdbe2f3
NikosTsigkros <tsigkrosn@gmail.com>
2026-01-07 02:53:05 +0200

    add custom roles and types

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,10 @@
 from flask import Flask
 
+ROLE_EMPLOYEE = "employee"
+ROLE_MANAGER = "manager"
+ROLE_ADMIN = "admin"
+ROLE_OPTIONS = [ROLE_EMPLOYEE, ROLE_MANAGER, ROLE_ADMIN]
+CUSTOMER_CATEGORIES = ["Lead", "Active", "Inactive", "Cancelled"]
 
 def create_app() -> Flask:
     app = Flask(__name__)
