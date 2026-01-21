78fb7828d2436ddf90886d32753d41a84aaec186
NikosTsigkros <tsigkrosn@gmail.com>
2026-01-22 00:43:00 +0200

    add /customers route get data functionality

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,8 @@
 from flask import Blueprint, redirect, render_template, url_for
-from flask_login import login_required, logout_user
+from flask_login import current_user, login_required, logout_user
+
+from app.constants import CUSTOMER_CATEGORIES, ROLE_EMPLOYEE
+from app.models import Customer
 
 web_bp = Blueprint("web", __name__)
 
@@ -17,4 +20,17 @@ def login():
 @login_required
 def logout():
     logout_user()
-    return redirect(url_for("crm.login"))
+    return redirect(url_for("web.login"))
+
+@web_bp.route("/customers")
+@login_required
+def customers():
+    query = Customer.query
+    if current_user.role == ROLE_EMPLOYEE:
+        query = query.filter_by(created_by_id=current_user.id)
+    customers_list = query.order_by(Customer.created_at.desc()).all()
+    return render_template(
+        "customers.html",
+        customers=customers_list,
+        categories=CUSTOMER_CATEGORIES,
+    )
